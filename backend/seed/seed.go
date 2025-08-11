db88a5b58361a1916ba52ce624141e2cc7549499
Eyob <eabebe91@gmail.com>
2025-08-11 10:08:15 +0300

    fix: admin added to seed also publish_posts permission with 2 other permissions

--- backend/seed/seed.go
+++ backend/seed/seed.go
@@ -2,6 +2,7 @@ package seed
 
 import (
 	"github.com/eyoba-bisru/blogly/backend/config"
+	"github.com/eyoba-bisru/blogly/backend/helpers"
 	"github.com/eyoba-bisru/blogly/backend/models"
 )
 
@@ -13,15 +14,28 @@ func SeedRolesAndPermissions() {
 	perm := models.Permission{Name: "read_posts"}
 	db.FirstOrCreate(&perm, perm)
 
+	perm2 := models.Permission{Name: "write_posts"}
+	db.FirstOrCreate(&perm2, perm2)
+
+	perm3 := models.Permission{Name: "delete_posts"}
+	db.FirstOrCreate(&perm3, perm3)
+
+	perm4 := models.Permission{Name: "publish_posts"}
+	db.FirstOrCreate(&perm4, perm4)
+
 	// Role
 	userRole := models.Role{Name: "user"}
 	db.FirstOrCreate(&userRole, userRole)
-	db.Model(&userRole).Association("Permissions").Append(&perm)
+	db.Model(&userRole).Association("Permissions").Append([]models.Permission{perm, perm2, perm3})
+
+	adminRole := models.Role{Name: "admin"}
+	db.FirstOrCreate(&adminRole, adminRole)
+	db.Model(&adminRole).Association("Permissions").Append([]models.Permission{perm, perm2, perm3, perm4})
 
 	// Category
-	category := models.Category{Name: "General", Slug: "general"}
+	category := models.Category{Name: "General", Slug: helpers.Slugify("general")}
 	db.FirstOrCreate(&category, category)
 
-	category2 := models.Category{Name: "Technology", Slug: "technology"}
+	category2 := models.Category{Name: "Technology", Slug: helpers.Slugify("technology")}
 	db.FirstOrCreate(&category2, category2)
 }
